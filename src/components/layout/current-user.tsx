ece732603787df8ed098e666c20b79831636c8be
Mukund Pai <mukundppai10@gmail.com>
2024-06-05 00:02:32 +0530

    Change account setting page.

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,76 +1,93 @@
-import { Popover , Button} from 'antd'
-import CustomAvatar from '../custom-avatar'
-import { useGetIdentity } from '@refinedev/core'
-import { Text } from '../text'
-import type { User } from '@/graphql/schema.types'
-import { useState } from 'react'
-import { SettingOutlined } from '@ant-design/icons'
-import { AccountSettings } from './account-settings'
+import { useState } from "react";
 
-const CurrentUser = () => {
-    const [IsOpen, setIsOpen] = useState(false)
-    const { data: user } = useGetIdentity<User>()
-    
-    const content = (
-        <div style= {{
-            display: 'flex',
-            flexDirection: 'column',
-        }}>
-            <Text
-             strong
-             style={{ padding:'12px 20px'}}
-            >
-                {user?.name}
-            </Text>
-            <div
-                style={{
-                borderTop: "1px solid #d9d9d9",
-                padding: "4px",
-                display: "flex",
-                flexDirection: "column",
-                gap: "4px",
-                }}
-            >
+import { useGetIdentity, useLogout } from "@refinedev/core";
 
-     
-                <Button
-                  style={{ textAlign: 'left'}}
-                  icon={<SettingOutlined />}
-                  type="text"
-                  block
-                  onClick={() => setIsOpen(true)}
-                >
-                    Account Settings
-                </Button>
-            </div>
-        </div>
-    )
- 
+import { LogoutOutlined, SettingOutlined } from "@ant-design/icons";
+import { Button, Popover } from "antd";
+
+import type { User } from "@/graphql/schema.types";
+
+import { CustomAvatar } from "../custom-avatar";
+import { Text } from "../text";
+import { AccountSettings } from "./account-settings";
+
+export const CurrentUser: React.FC = () => {
+  const [opened, setOpened] = useState(false);
+  const { data: user } = useGetIdentity<User>();
+  const { mutate: logout } = useLogout();
+
+  const content = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Text
+        strong
+        style={{
+          padding: "12px 20px",
+        }}
+      >
+        {user?.name}
+      </Text>
+      <div
+        style={{
+          borderTop: "1px solid #d9d9d9",
+          padding: "4px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <Button
+          style={{ textAlign: "left" }}
+          // @ts-expect-error Ant Design Icon's v5.0.1 has an issue with @types/react@^18.2.66
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setOpened(true)}
+        >
+          Account settings
+        </Button>
+        <Button
+          style={{ textAlign: "left" }}
+          // @ts-expect-error Ant Design Icon's v5.0.1 has an issue with @types/react@^18.2.66
+          icon={<LogoutOutlined />}
+          type="text"
+          danger
+          block
+          onClick={() => logout()}
+        >
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
-        <Popover
-            placement ='bottomRight'
-            trigger="click"
-            overlayInnerStyle={{ padding:0 }}
-            overlayStyle={{ zIndex: 999}}
-            content={content}
-        >
-            <CustomAvatar 
-                name={user?.name}
-                src={user?.avatarUrl}
-                size="default"
-                style={{ cursor: 'pointer'}}
-            />
-        </Popover>
-        {user && (
-            <AccountSettings
-            opened={IsOpen}
-            setOpened={setIsOpen}
-            userId={user.id}
-            />
-        )}
+      <Popover
+        placement="bottomRight"
+        content={content}
+        trigger="click"
+        overlayInnerStyle={{ padding: 0 }}
+        overlayStyle={{ zIndex: 999 }}
+      >
+        <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size="default"
+          style={{ cursor: "pointer" }}
+        />
+      </Popover>
+      {user && (
+        <AccountSettings
+          opened={opened}
+          setOpened={setOpened}
+          userId={user.id}
+        />
+      )}
     </>
-  )
-}
-
-export default CurrentUser;
+  );
+};
